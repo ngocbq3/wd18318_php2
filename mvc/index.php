8a1ee63f58a7b7b144861610057d2f155b77cc71
ngocbq3 <ngocbq@fpt.edu.vn>
2024-01-23 10:25:19 +0700

    add file .htaccess

--- mvc/index.php
+++ mvc/index.php
@@ -4,18 +4,4 @@ require_once __DIR__ . "/config.php";
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-use App\Models\ProductModel;
-
-// $product = ProductModel::where('name', 'LIKE', '%iphone%')
-//     ->andWhere('price', '>=', 1000)
-//     ->get();
-
-// dd($product);
-$data = [
-    'name' => 'Iphone 20',
-    'price' => 2500,
-    'detail' => '1 Quả thận',
-    'image' => "iphone20.jpg"
-];
-// dd(ProductModel::insert($data));
-dd(ProductModel::update(168, $data));
+echo "HOME";
